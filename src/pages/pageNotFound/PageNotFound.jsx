b8876cefbb8c77353caa09266a30f50d38726ba5
Dione Martins <profissionaldione@gmail.com>
2022-12-18 18:33:48 -0300

    Adicionando PageNotFound

--- src/pages/pageNotFound/PageNotFound.jsx
+++ src/pages/pageNotFound/PageNotFound.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import { Header } from "../../components";
 import style from './PageNotFound.module.css';
 
-const { notFoundText } = style;
+const { notFoundText, notFoundTextMini } = style;
 
 export default function PageNotFound(){
     return(
         <div>
             <Header/>
             <h1 className={notFoundText}>Página não encontrada</h1>
+            <h2 className={notFoundTextMini}>Alguém está tentando bisbilhotar os arquivos do mestre...?</h2>
         </div>
     );
 }
